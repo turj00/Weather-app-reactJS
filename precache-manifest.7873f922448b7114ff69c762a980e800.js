8a8b7fd99138d4afdb7825ee894ac7e20144352b
turj00 <turjo1331@gmail.com>
2020-09-01 05:08:17 +0600

    Updates

---
+++ precache-manifest.7873f922448b7114ff69c762a980e800.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60917e1e49a39c3b1815b79ad612a6c4",
+    "url": "/Weather-app-reactJS/index.html"
+  },
+  {
+    "revision": "2b457e4b111df90852cd",
+    "url": "/Weather-app-reactJS/static/css/2.0ecd484c.chunk.css"
+  },
+  {
+    "revision": "87628ce39d13da0bfa98",
+    "url": "/Weather-app-reactJS/static/css/main.7ac4d74e.chunk.css"
+  },
+  {
+    "revision": "2b457e4b111df90852cd",
+    "url": "/Weather-app-reactJS/static/js/2.d55887a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app-reactJS/static/js/2.d55887a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87628ce39d13da0bfa98",
+    "url": "/Weather-app-reactJS/static/js/main.a401e703.chunk.js"
+  },
+  {
+    "revision": "a327343a35b421dc3d42",
+    "url": "/Weather-app-reactJS/static/js/runtime-main.90a835a0.js"
+  },
+  {
+    "revision": "356bfbfdff135113dfe186a42a7a2904",
+    "url": "/Weather-app-reactJS/static/media/background2.356bfbfd.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Weather-app-reactJS/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Weather-app-reactJS/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Weather-app-reactJS/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Weather-app-reactJS/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/Weather-app-reactJS/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
